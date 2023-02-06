ca00031e77550373ce013c2bbe3963c0ee24c9cb
gorkemyar <gorkemyar2000@hotmail.com>
2023-02-06 10:28:28 -0500

    task 3 done

--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -10,3 +10,34 @@ print("[import ./../assign02.py] is done!")
 # mylist_mergesort (see list_mergesort in assign02.sml)
 #
 ####################################################
+
+
+def mylist_mergesort(xs):
+    def split(xs):
+        if xs.ctag <= 0:
+            return mylist_nil(), mylist_nil()
+        else:
+            if xs.cons2.ctag <= 0:
+                return mylist_cons(xs.cons1, mylist_nil()), mylist_nil()
+            else:
+                ys, zs = split(xs.cons2.cons2)
+                return mylist_cons(xs.cons1, ys), mylist_cons(xs.cons2.cons1, zs)
+    def merge(ys, zs):
+        if ys.ctag <= 0:
+            return zs
+        elif zs.ctag <= 0:
+            return ys
+        else:
+            if (ys.cons1 <= zs.cons1):
+                return mylist_cons(ys.cons1, merge(ys.cons2, zs))
+            else:
+                return mylist_cons(zs.cons1, merge(ys, zs.cons2))
+    
+    if xs.ctag <= 0:
+        return mylist_nil()
+    else:
+        if xs.cons2.ctag <= 0:
+            return mylist_cons(xs.cons1, mylist_nil())
+        else:
+            ys, zs = split(xs.cons2.cons2)
+            return merge(mylist_mergesort(mylist_cons(xs.cons1, ys)), mylist_mergesort(mylist_cons(xs.cons2.cons1,zs)))
