8c61542fc585fb4e4074ee9666043597b9c926a1
gorkemyar <gorkemyar2000@hotmail.com>
2023-03-23 16:43:44 -0400

    quiz

--- quizzes/part-03-23/part_03_23.py
+++ quizzes/part-03-23/part_03_23.py
@@ -55,16 +55,28 @@ def image_copy(image):
         imgvec.image_make_pylist\
         (hh, ww, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image, ij//ww, ij%ww)))
 ####################################################
-# def image_rotate_090r(image):
-#     ww = image.width
-#     hh = image.height
-#     return \
-#         imgvec.image_make_pylist\
-#         (??, ??, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image, ??????, ??????)))
+def image_rotate_090r(image):
+    ww = image.width
+    hh = image.height
+    return \
+        imgvec.image_make_pylist\
+        (ww, hh, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image, (hh-1-(ij%ww)), ij//hh)))
 ####################################################
-# balloons = \
-#     load_color_image\
-#     ("./../../assigns/05/MySolution/Python/INPUT/balloons.png")
-# save_color_image(image_hreflect(balloons), "balloons_copy.png")
-# save_color_image(image_rotate_090r(balloons), "balloons_090r.png")
+balloons = \
+    load_color_image\
+    ("./../../assigns/05/MySolution/Python/INPUT/balloons.png")
+#save_color_image(image_hreflect(balloons), "balloons_copy.png")
+save_color_image(image_rotate_090r(balloons), "balloons_090r.png")
 ####################################################
+
+# 0 1 2  3 4 5
+# 5 6 7 8 9
+# 10 11 12 13 14
+
+# 10 5 0  11 6 1
+# 12 7 2
+# 13 8 3
+# 14 9 4
+
+
+
